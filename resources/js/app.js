2ab9cdbfff6b129b99c2fbef362af04b4706602a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-08-08 17:04:42 +0300

    rev: reverting back

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,6 @@
 import './bootstrap';
 // import './schedule-modal'
-import './main'
 import './modal-handler'
-
-$.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
-
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
